512aeafa0df01b6be663bf8ef2f76e1d816e6b13
ttoommxx <ttoommxx@gmail.com>
2024-02-14 21:53:54 +0800

    reverted so much, but this works well

--- mangalife_downloader.py
+++ mangalife_downloader.py
@@ -32,35 +32,9 @@ def printer(manga: str, printing_queue: multiprocessing.Manager().Queue, number_
         print("No chapter has failed")
 
 
-<<<<<<< HEAD
-def download_and_zip(manga_title: str, chapter_number: int, chapter_path: str, printing_queue) -> bool:
-    """ given path and chapter_path, create the zip file
-    add a token to the queue when the process is done """
-
-    page_number = 1
-    url_chapter = f"https://official.lowee.us/manga/{manga_title}/{chapter_number:04d}-{page_number:03d}.png"
-    while True:
-        response = requests.get(url_chapter, stream=True, timeout=10)
-        if response.status_code != 200:
-            break
-
-        file_path = os.path.join(chapter_path, f"{page_number:03d}")
-        if os.path.exists(file_path):
-            continue
-        # Open the file in binary write mode
-        with open(file_path, "wb") as page:
-            for chunk in response.iter_content(1024):
-                page.write(chunk)
-
-        page_number += 1
-        url_chapter = f"https://official.lowee.us/manga/{manga_title}/{chapter_number:04d}-{page_number:03d}.png"
-
-    zip_path = os.join(os.path.dirname(chapter_path), chapter_number)
-=======
 def download_and_zip(zip_path: str, chapter_path: str, printing_queue: multiprocessing.Manager().Queue) -> bool:
     """ given path and chapter_path, create the zip file
     add a token to the queue when the process is done """
->>>>>>> parent of 7317fc3 (bit messy actually, need to reverse engineer more of the javascript included)
     with ZipFile(zip_path, "a") as zip_file:
         pages = os.listdir(chapter_path)
         for page in pages:
@@ -77,30 +51,41 @@ def main() -> None:
     """ main function """
     
     url = ARGS.url
-    manga_title = url.split("/")[-1]
-    html_string = requests.get(url).text
-    chapters_string = re.findall(
-        r"vm.Chapters = (.*);", html_string)[0].replace("null", "None")
+    manga_name = url.split("/")[-1]
+    html_string = requests.get(url, timeout=30).text
+    chapters_string = re.findall(r"vm.Chapters = (.*);", html_string)[0].replace("null", "None")
     list_chapters = ast.literal_eval(chapters_string)
 
 
-    # for chapter in list_chapters:
-    #     chapter_number = int(chapter["Chapter"][1:-1])
-    #     page_number = 1
-    #     while True:
-    #         url_chapter = f"https://www.manga4life.com/read-online/{
-    #             MANGA}-chapter-{chapter_number}-page-{page_number}.html"
-    #         page_string = requests.get(url_chapter).text
-    #         if "404 Page Not Found" in page_string:
-    #             break
+    folder_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "MANGAS", manga_name)
+    os.makedirs(folder_path, exist_ok=True)
 
-    #         # here do stuff
+    for chapter in list_chapters:
+        chapter_number = f"{int(chapter["Chapter"][1:-1]):04d}"
+        if chapter["Chapter"][-1] != "0":
+            chapter_number += "." + str(chapter["Chapter"][-1])
 
-    #         page_number += 1
+        chapter_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "MANGAS", manga_name, chapter_number)
+        if os.path.exists(f"{chapter_path}.cbz"):
+            continue
+        os.makedirs(chapter_path, exist_ok=True)
 
-    #     break
+        page_number = 0
+        while True:
+            page_number += 1
 
+            url_chapter = f"https://official.lowee.us/manga/{manga_name}/{chapter_number}-{page_number:03d}.png"
+            response = requests.get(url_chapter, stream=True, timeout=10)
+            if response.status_code != 200:
+                break
 
+            file_path = os.path.join(chapter_path, f"{page_number:03d}")
+            if os.path.exists(file_path):
+                continue
+            # Open the file in binary write mode
+            with open(file_path, "wb") as page:
+                for chunk in response.iter_content(1024):
+                    page.write(chunk)
 
     return
 
