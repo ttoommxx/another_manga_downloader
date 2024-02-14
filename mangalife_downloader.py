fab4fa2aa8963b1c7390ea194b5f5c16d387a002
ttoommxx <ttoommxx@gmail.com>
2024-02-14 21:20:02 +0800

    tt

--- mangalife_downloader.py
+++ mangalife_downloader.py
@@ -10,7 +10,7 @@ from zipfile import ZipFile
 
 
 parser = argparse.ArgumentParser(prog="mangalife_downloader", description="download manga from mangalife")
-parser.add_argument("name")
+parser.add_argument("url")
 ARGS = parser.parse_args() # args.picker contains the modality
 
 
@@ -32,6 +32,7 @@ def printer(manga: str, printing_queue: multiprocessing.Manager().Queue, number_
         print("No chapter has failed")
 
 
+<<<<<<< HEAD
 def download_and_zip(manga_title: str, chapter_number: int, chapter_path: str, printing_queue) -> bool:
     """ given path and chapter_path, create the zip file
     add a token to the queue when the process is done """
@@ -55,6 +56,11 @@ def download_and_zip(manga_title: str, chapter_number: int, chapter_path: str, p
         url_chapter = f"https://official.lowee.us/manga/{manga_title}/{chapter_number:04d}-{page_number:03d}.png"
 
     zip_path = os.join(os.path.dirname(chapter_path), chapter_number)
+=======
+def download_and_zip(zip_path: str, chapter_path: str, printing_queue: multiprocessing.Manager().Queue) -> bool:
+    """ given path and chapter_path, create the zip file
+    add a token to the queue when the process is done """
+>>>>>>> parent of 7317fc3 (bit messy actually, need to reverse engineer more of the javascript included)
     with ZipFile(zip_path, "a") as zip_file:
         pages = os.listdir(chapter_path)
         for page in pages:
@@ -70,30 +76,70 @@ def download_and_zip(manga_title: str, chapter_number: int, chapter_path: str, p
 def main() -> None:
     """ main function """
     
-    # fetch html file
-    manga_name = ARGS.name
+    url = ARGS.url
+    manga_title = url.split("/")[-1]
+    html_string = requests.get(url).text
+    chapters_string = re.findall(
+        r"vm.Chapters = (.*);", html_string)[0].replace("null", "None")
+    list_chapters = ast.literal_eval(chapters_string)
 
-    manga_folder = os.path.join(os.path.dirname(
-        os.path.realpath(__file__)), "MANGAS", manga_name)
 
-    if os.path.exists(manga_folder):
-        print("Folder already exists, adding new chapters")
-    else:
-        os.mkdir(manga_folder)
+    # for chapter in list_chapters:
+    #     chapter_number = int(chapter["Chapter"][1:-1])
+    #     page_number = 1
+    #     while True:
+    #         url_chapter = f"https://www.manga4life.com/read-online/{
+    #             MANGA}-chapter-{chapter_number}-page-{page_number}.html"
+    #         page_string = requests.get(url_chapter).text
+    #         if "404 Page Not Found" in page_string:
+    #             break
 
-    number_chapters = 
+    #         # here do stuff
+
+    #         page_number += 1
+
+    #     break
+
+
+
     return
+
+
+    # essential variables
+    manga = os.path.basename(folder_path)
+    cbz_folder_path = f"{folder_path}_CBZ"
+
+    if os.path.exists(cbz_folder_path):
+        if not os.path.isdir(cbz_folder_path):
+            sys.exit(f"File with the name {manga}_CBZ already exists, rename and rerun the program")
+        print("The folder alredy exists, adding missing files")
+    else:
+        os.mkdir(cbz_folder_path)
 
     # printing queue for communicating between the printer function and the pool
     printing_queue = multiprocessing.Manager().Queue()
+
+    # create a list of chapters
+    list_chapters = []
+    for chapter in os.listdir(folder_path):
+        chapter_path = os.path.join(folder_path, chapter)
+        if not os.path.isdir( chapter_path ):
+            continue # nothing to do
+
+        zip_path = os.path.join(cbz_folder_path, f"{chapter}.cbz")
+        if os.path.exists(zip_path):
+            continue # skip if the file already exits
+
+        list_chapters.append((zip_path, chapter_path, printing_queue))
+
     pool = multiprocessing.Pool()
 
     # set up the printing function
-    pool.apply_async(printer, (manga_name, printing_queue, number_chapters))
+    number_chapters = len(list_chapters)
+    pool.apply_async(printer, (manga, printing_queue, number_chapters))
 
-    # set up the downloading and zipping process
-    list_chapters = list(range(1, number_chapters+1))
-    pool.starmap(download_and_zip, list_chapters)
+    # set up the zipper processing of the chapters
+    pool.starmap(zipper, list_chapters)
 
     pool.close()
     pool.join()
