8e0fd5001966f21eea41a31cf78dd206069afcaa
= <=>
2024-02-26 09:49:18 +0000

    small changes, but not working still

--- manga_websites.py
+++ manga_websites.py
@@ -218,13 +218,19 @@ class Manganato:
         response = requests.get(chapter, timeout=10)
         if response.status_code == 200:
             html_string = response.text
-            images = re.findall('<img src="(.*?)(?<=\.jpg)"', html_string)
+            images = re.findall(r'<img src="(.*?)(?<=\.jpg)"', html_string)
 
         for image_link in images:
-            print(image_link)
-            response = requests.get(image_link, stream=True, timeout=10)
-            page_number = image_link.split("/")[-1].split("-")[0]
-            yield f"{page_number:03}", response
+            headers = {
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                "Accept-Language": "en-US,en;q=0.9",
+            }
+            response = requests.get(
+                image_link, stream=True, timeout=10, headers=headers
+            )
+            page_number = int(image_link.split("/")[-1].split("-")[0])
+            yield f"{page_number:03d}", response
 
 
 class Manga:
